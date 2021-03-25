dd09650442377d6000e2c468321c4b8dcbb675d5
Stephen Makilan <smakilan@m3-tech.ca>
2021-03-25 21:35:57 +0800

    first commit

---
+++ Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace M3ESignService
+{
+    public enum CodeResult
+    {
+        Success,
+        Failed,
+        NoData
+    }
+}
---
+++ Excel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Excel = Microsoft.Office.Interop.Excel.Application;
+
+namespace M3ESignService
+{
+    public class ExcelService
+    {
+        private readonly Excel oExcel = new Excel();
+        public CodeResult Export()
+        {
+            var cr = CodeResult.NoData;
+            //add workbook to excel
+            var oWorkBook = oExcel.Workbooks.Add();
+
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
